c13959a2aa926ab6e93274eec865ee32b05b95df
陈永达 <chenyongda3@xiaomi.com>
2024-07-22 23:23:04 +0800

    [update]rust每日练习

--- Misc-Projects/practice/src/main.rs
+++ Misc-Projects/practice/src/main.rs
@@ -1,5 +1,13 @@
+
 fn main() {
-    test_string3();
+    test_struct();
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_account: u64,
 }
 
 // fn test_String() {
@@ -15,12 +23,29 @@ fn main() {
 // }
 
 
-fn test_string3() {
-    let s = String::from("hello");
-    takes_ownership(s);
-    println!("在move进函数后继续使用s,{}",s);
-}
+// fn test_string3() {
+//     let s = String::from("hello");
+//     takes_ownership(s);
+//     println!("在move进函数后继续使用s,{}",s);
+// }
+//
+// fn takes_ownership(some_string:String) {
+//     println!("{}",some_string);
+// }
 
-fn takes_ownership(some_string:String) {
-    println!("{}",some_string);
+
+// fn test_tuple() {
+//     let x = (1,"h",1.1);
+//     let (q,w,e) = x;
+//     println!("{},{}",x.0,q);
+// }
+
+fn test_struct() {
+    let user = User {
+        active: true,
+        username: String::from("zhangsan"),
+        email: String::from("123@qq.com"),
+        sign_in_account:123
+    };
+    println!("{}",user.active)
 }
